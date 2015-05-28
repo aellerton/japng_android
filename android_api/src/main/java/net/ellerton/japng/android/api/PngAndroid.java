ed28943cd4ec07fa88886b7e129bdfa00fa62b09
Andrew E <andrewjellerton@gmail.com>
2015-05-28 21:41:18 +0800

    add minimal activity and api

---
+++ android_api/src/main/java/net/ellerton/japng/android/api/PngAndroid.java
@@ -0,0 +1,12 @@
+package net.ellerton.japng.android.api;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by aellerton on 28/05/2015.
+ */
+public class PngAndroid {
+    public static Bitmap toBitmap() {
+        return null;
+    }
+}
---
+++ android_demo/src/main/java/net/ellerton/japng/android/demo/PngIndexActivity.java
@@ -0,0 +1,42 @@
+package net.ellerton.japng.android.demo;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import net.ellerton.japng.android.api.PngAndroid;
+
+
+public class PngIndexActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_png_index);
+        Bitmap nothing = PngAndroid.toBitmap(); // just proving it works
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_png_index, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
